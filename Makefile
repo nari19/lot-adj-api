21e358e07f67a5876f9b410828f3f10a402ef631
nari19 <kazuki7793996@gmail.com>
2025-06-04 09:43:59 +0900

    f

--- Makefile
+++ Makefile
@@ -11,4 +11,4 @@ test-predict:
 		'http://127.0.0.1:8000/predict/' \
 		-H 'accept: application/json' \
 		-H 'Content-Type: application/json' \
-		-d '{"symbol": "EURJPY", "data": [{"open": 100.0, "high": 101.0, "low": 99.0, "close": 100.5}]}'
+		-d @test_data.json
---
+++ convert_to_json.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import json
+
+# CSVデータを読み込む
+df = pd.read_csv('data.csv')
+
+# 必要なカラムだけを選択
+df = df[['open', 'high', 'low', 'close']]
+
+# 最初の600行を取得
+df = df.head(600)
+
+# データを辞書のリストに変換
+data = df.to_dict('records')
+
+# JSON形式に変換
+json_data = {
+    "symbol": "EURUSD",
+    "data": data
+}
+
+# JSONファイルに書き込む
+with open('test_data.json', 'w') as f:
+    json.dump(json_data, f, indent=2) 
